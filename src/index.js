38a7cd4ff7a06c251baf37227b76dc8f0028aed0
ajin3830 <ajin3830@gmail.com>
2023-01-18 14:18:41 -0500

    completed lab

--- src/index.js
+++ src/index.js
@@ -1,25 +1,47 @@
 // write your code here
 
-// See all ramen images in the div with the id of ramen-menu. 
-// When the page loads, request the data from the server to get all the ramen objects. 
-// Then, display the image for each of the ramen using an img tag inside the #ramen-menu div.
-
-fetch("http://localhost:3000/ramens") 
-.then(res => res.json())
-.then(data => renderRamens(data))
-
-function renderRamens(ramens) {
-    ramens.forEach((ramen) => {
-        const ramenMenu = document.querySelector('#ramen-menu')
-
-        const img = document.createElement('img')
-        img.src = ramen.img
-
-        document.getElementById('ramen-menu').appendChild(img)
-        
-        document.querySelector('img').addEventListener('click', (e) => {console.log(hehe)})
-})
+function getRamen() {
+    fetch("http://localhost:3000/ramens") 
+    .then(res => res.json())
+    .then(ramens => ramens.forEach(renderRamen))
 }
 
-// Click on an image from the #ramen-menu div and see all the info about that ramen displayed 
-// inside the #ramen-detail div and where it says insert comment here and insert rating here.
+getRamen()
+
+function renderRamen(ramen) {
+    
+    // const ramenMenu = document.querySelector('#ramen-menu')
+    const div = document.createElement('div')
+    div.className = 'ramen'
+
+    const img = document.createElement('img')
+    img.src = ramen.image
+    img.className = 'ramen-img'
+    img.addEventListener('click', (e) => {
+        document.querySelector("#ramen-detail > img").src = ramen.image
+        document.querySelector("#ramen-detail > h2").textContent = ramen.name
+        document.querySelector("#ramen-detail > h3").textContent = ramen.restaurant
+        document.querySelector("body > h3:nth-child(4)").textContent = ramen.rating
+        document.querySelector("#comment-display").textContent = ramen.comment
+    })
+    
+    div.appendChild(img)
+    document.getElementById('ramen-menu').appendChild(div)
+             
+}
+
+const form = document.querySelector('form')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const newRamen = {
+        name: e.target.name.value,
+        restaurant: e.target.restaurant.value,
+        image: e.target.image.value,
+        rating: e.target.rating.value,
+        comment: e.target["new-comment"].value
+    } 
+    renderRamen(newRamen)
+})
+
+
